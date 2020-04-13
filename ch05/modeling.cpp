ff678544965a57d09e8b98c3b44c7003779a4632
seungCheolCha <cscculture95@gmail.com>
2020-04-14 00:08:47 +0900

    GL glut practice 14.04.20

--- ch05/modeling.cpp
+++ ch05/modeling.cpp
@@ -20,10 +20,100 @@ void InitLight()
 	GLfloat mat_specular[] = { 1.0,1.0,1.0,1.0 };
 	GLfloat mat_ambient[] = { 0.5,0.4,0.3,1.0 };
 	GLfloat mat_shininess[] = { 15.0 };
+	GLfloat light_specular[] = { 1.0,1.0,1.0,1.0 };
+	GLfloat light_diffuse[] = { 0.8,0.8,0.8,1.0 };
+	GLfloat light_ambient[] = { 0.3,0.3,0.3,1.0 };
+	GLfloat light_position[] = { -3,6,3.0,0.0 };
+
+	glShadeModel(GL_SMOOTH);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+
+	/* LIGHT0 조명의 어트리뷰트 설정*/
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+}
+
+void mouseMove(GLint X, GLint Y)
+{
+	// 마우스 움직임 X, Y를 전역 변수인 ViewX, ViewY에 할당
+	glutPostRedisplay();
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'q' : case 'Q' : case '\033':
+		exit(0);
+		break;
+	case 's' : case 'S' : 
+		if (flatShaded)
+		{
+			flatShaded = 0;
+			glShadeModel(GL_SMOOTH);
+		}
+		else
+		{
+			flatShaded = 1;
+			glShadeModel(GL_FLAT);
+		}
+		glutPostRedisplay();
+		break;
+		// 여기에 와이어 프레임 토글링을 위한 코드를 삽입할 것
+		// 위의 전역 변수 Wireframed를 사용할 것
+
+	default:
+		break;
+	}
+}
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	// 이 함수는 시점을 나타내는 함수로
+	// mouseMove 함수의 변화에 따라서 변화량을 변경해볼것
+	gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 1.0, 1.0, 0.0);
+	glutSolidTeapot(0.2);
+	glFlush();
+}
+
+void reshape(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
 }
 
 int main(int argc, char ** argv)
 {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH);
+	glutInitWindowSize(400, 400);
+	glutInitWindowPosition(400, 400);
+	glutCreateWindow("Modeling practice");
+	glClearColor(0.4, 0.4, 0.4, 0.0);
+	InitLight();
 
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
+	glutMotionFunc(mouseMove);
+	glutReshapeFunc(reshape);
+
+	glutMainLoop();
+
+	return 0;
 
 }
