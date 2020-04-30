ef9a90f7c273d44c47447bda3e6b4b3d12c0b3e7
seungCheolCha <cscculture95@gmail.com>
2020-04-30 23:19:10 +0900

    study about ray tracing

---
+++ ch13/recursive_rayTracing.cpp
@@ -0,0 +1,25 @@
+#include <glut.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+class Point
+{
+public : 
+
+};
+
+class Vector
+{
+
+};
+
+void TraceRay(Point start, Vector direction)
+{
+
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	
+}
